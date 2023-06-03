6a8f21bad19c458cf9681dfd7336829a4d6741f4
Daniel Augusto Krug <daniel.krug@fepese.org.br>
2023-06-03 12:19:40 -0300

    feat: adding mnesia utilitary module

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Krug.MixProject do
   def project do
     [
       app: :krug,
-      version: "1.1.16",
+      version: "1.1.17",
       elixir: "~> 1.13",
       source_url: @project_url,
       homepage_url: @project_url,
