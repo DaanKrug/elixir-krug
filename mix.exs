ec5f9bd91620a083fd746b5cd78509a9ddbfb421
Daniel Augusto Krug <daniel.krug@fepese.org.br>
2023-06-11 21:50:14 -0300

    feat: released version 1.1.26

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Krug.MixProject do
   def project do
     [
       app: :krug,
-      version: "1.1.25",
+      version: "1.1.26",
       elixir: "~> 1.13",
       source_url: @project_url,
       homepage_url: @project_url,
