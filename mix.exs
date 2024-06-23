29caaa0995fe4b548663e90daab56d9a47ea411d
Daniel Augusto Krug <daniel.krug@fepese.org.br>
2024-06-23 15:49:35 -0300

    fix: adjusting version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Krug.MixProject do
   def project do
     [
       app: :krug,
-      version: "2.0.12",
+      version: "2.0.13",
       elixir: "~> 1.13",
       source_url: @project_url,
       homepage_url: @project_url,
